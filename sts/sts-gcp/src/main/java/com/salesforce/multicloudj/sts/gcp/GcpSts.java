678185eead3a19146e7050ff6147d77f5351e2b4
Yamini Kashyap <yaminik1996@gmail.com>
2025-05-22 14:39:00 -0700

    Implement exception handling

--- sts/sts-gcp/src/main/java/com/salesforce/multicloudj/sts/gcp/GcpSts.java
+++ sts/sts-gcp/src/main/java/com/salesforce/multicloudj/sts/gcp/GcpSts.java
@@ -1,5 +1,7 @@
 package com.salesforce.multicloudj.sts.gcp;
 
+import com.google.api.gax.rpc.ApiException;
+import com.google.api.gax.rpc.StatusCode;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.auto.service.AutoService;
 import com.google.cloud.iam.credentials.v1.GenerateAccessTokenRequest;
@@ -7,7 +9,16 @@ import com.google.cloud.iam.credentials.v1.GenerateAccessTokenResponse;
 import com.google.cloud.iam.credentials.v1.IamCredentialsClient;
 import com.google.protobuf.Duration;
 
+import com.salesforce.multicloudj.common.exceptions.DeadlineExceededException;
+import com.salesforce.multicloudj.common.exceptions.FailedPreconditionException;
+import com.salesforce.multicloudj.common.exceptions.InvalidArgumentException;
+import com.salesforce.multicloudj.common.exceptions.ResourceAlreadyExistsException;
+import com.salesforce.multicloudj.common.exceptions.ResourceExhaustedException;
+import com.salesforce.multicloudj.common.exceptions.ResourceNotFoundException;
 import com.salesforce.multicloudj.common.exceptions.SubstrateSdkException;
+import com.salesforce.multicloudj.common.exceptions.UnAuthorizedException;
+import com.salesforce.multicloudj.common.exceptions.UnknownException;
+import com.salesforce.multicloudj.common.exceptions.UnSupportedOperationException;
 import com.salesforce.multicloudj.sts.driver.AbstractSts;
 import com.salesforce.multicloudj.sts.model.AssumedRoleRequest;
 import com.salesforce.multicloudj.sts.model.CallerIdentity;
@@ -15,7 +26,9 @@ import com.salesforce.multicloudj.sts.model.GetAccessTokenRequest;
 import com.salesforce.multicloudj.sts.model.StsCredentials;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @SuppressWarnings("rawtypes")
 @AutoService(AbstractSts.class)
@@ -28,7 +41,7 @@ public class GcpSts extends AbstractSts<GcpSts> {
         try {
             stsClient = IamCredentialsClient.create();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new SubstrateSdkException("Could not create IAM client ", e);
         }
     }
 
@@ -48,7 +61,6 @@ public class GcpSts extends AbstractSts<GcpSts> {
                 .addAllScope(List.of(scope))
                 .setLifetime(Duration.newBuilder().setSeconds(request.getExpiration()).build())
                 .build();
-
         GenerateAccessTokenResponse response = this.stsClient.generateAccessToken(accessTokenRequest);
         return new StsCredentials(null, null, response.getAccessToken());
     }
@@ -60,7 +72,7 @@ public class GcpSts extends AbstractSts<GcpSts> {
             credentials.refreshIfExpired();
             return new CallerIdentity(null, credentials.getAccessToken().getTokenValue(), null);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new SubstrateSdkException("Could not create credentials in given environment", e);
         }
     }
 
@@ -71,7 +83,7 @@ public class GcpSts extends AbstractSts<GcpSts> {
             credentials.refreshIfExpired();
             return new StsCredentials(null, null, credentials.getAccessToken().getTokenValue());
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new SubstrateSdkException("Could not create credentials in given environment", e);
         }
     }
 
@@ -82,7 +94,32 @@ public class GcpSts extends AbstractSts<GcpSts> {
 
     @Override
     public Class<? extends SubstrateSdkException> getException(Throwable t) {
-        return null;
+        if (t instanceof ApiException) {
+            ApiException exception = (ApiException) t;
+            StatusCode statusCode = exception.getStatusCode();
+
+            Map<StatusCode.Code, Class<? extends SubstrateSdkException>> ERROR_MAP = new HashMap<>() {{
+                put(StatusCode.Code.CANCELLED, UnknownException.class);
+                put(StatusCode.Code.UNKNOWN, UnknownException.class);
+                put(StatusCode.Code.INVALID_ARGUMENT, InvalidArgumentException.class);
+                put(StatusCode.Code.DEADLINE_EXCEEDED, DeadlineExceededException.class);
+                put(StatusCode.Code.NOT_FOUND, ResourceNotFoundException.class);
+                put(StatusCode.Code.ALREADY_EXISTS, ResourceAlreadyExistsException.class);
+                put(StatusCode.Code.PERMISSION_DENIED, UnAuthorizedException.class);
+                put(StatusCode.Code.RESOURCE_EXHAUSTED, ResourceExhaustedException.class);
+                put(StatusCode.Code.FAILED_PRECONDITION, FailedPreconditionException.class);
+                put(StatusCode.Code.ABORTED, DeadlineExceededException.class);
+                put(StatusCode.Code.OUT_OF_RANGE, InvalidArgumentException.class);
+                put(StatusCode.Code.UNIMPLEMENTED, UnSupportedOperationException.class);
+                put(StatusCode.Code.INTERNAL, UnknownException.class);
+                put(StatusCode.Code.UNAVAILABLE, UnknownException.class);
+                put(StatusCode.Code.DATA_LOSS, UnknownException.class);
+                put(StatusCode.Code.UNAUTHENTICATED, UnAuthorizedException.class);
+            }};
+
+            return ERROR_MAP.getOrDefault(statusCode.getCode(), UnknownException.class);
+        }
+        return UnknownException.class;
     }
 
     public static class Builder extends AbstractSts.Builder<GcpSts> {
--- sts/sts-gcp/src/test/java/com/salesforce/multicloud/sts/gcp/GcpStsTest.java
+++ sts/sts-gcp/src/test/java/com/salesforce/multicloud/sts/gcp/GcpStsTest.java
@@ -5,6 +5,18 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.iam.credentials.v1.GenerateAccessTokenRequest;
 import com.google.cloud.iam.credentials.v1.GenerateAccessTokenResponse;
 import com.google.cloud.iam.credentials.v1.IamCredentialsClient;
+import com.google.api.gax.rpc.ApiException;
+import com.google.api.gax.rpc.StatusCode;
+import com.salesforce.multicloudj.common.exceptions.DeadlineExceededException;
+import com.salesforce.multicloudj.common.exceptions.FailedPreconditionException;
+import com.salesforce.multicloudj.common.exceptions.InvalidArgumentException;
+import com.salesforce.multicloudj.common.exceptions.ResourceAlreadyExistsException;
+import com.salesforce.multicloudj.common.exceptions.ResourceExhaustedException;
+import com.salesforce.multicloudj.common.exceptions.ResourceNotFoundException;
+import com.salesforce.multicloudj.common.exceptions.SubstrateSdkException;
+import com.salesforce.multicloudj.common.exceptions.UnAuthorizedException;
+import com.salesforce.multicloudj.common.exceptions.UnknownException;
+import com.salesforce.multicloudj.common.exceptions.UnSupportedOperationException;
 import com.salesforce.multicloudj.sts.gcp.GcpSts;
 import com.salesforce.multicloudj.sts.model.AssumedRoleRequest;
 import com.salesforce.multicloudj.sts.model.CallerIdentity;
@@ -117,5 +129,46 @@ public class GcpStsTest {
                 new GcpSts(new GcpSts().builder());
             });
         }
+    }
+
+    @Test
+    public void TestGetExceptionWithApiException() {
+        GcpSts sts = new GcpSts().builder().build(mockStsClient);
+        
+        // Test various status codes
+        assertExceptionMapping(sts, StatusCode.Code.CANCELLED, UnknownException.class);
+        assertExceptionMapping(sts, StatusCode.Code.UNKNOWN, UnknownException.class);
+        assertExceptionMapping(sts, StatusCode.Code.INVALID_ARGUMENT, InvalidArgumentException.class);
+        assertExceptionMapping(sts, StatusCode.Code.DEADLINE_EXCEEDED, DeadlineExceededException.class);
+        assertExceptionMapping(sts, StatusCode.Code.NOT_FOUND, ResourceNotFoundException.class);
+        assertExceptionMapping(sts, StatusCode.Code.ALREADY_EXISTS, ResourceAlreadyExistsException.class);
+        assertExceptionMapping(sts, StatusCode.Code.PERMISSION_DENIED, UnAuthorizedException.class);
+        assertExceptionMapping(sts, StatusCode.Code.RESOURCE_EXHAUSTED, ResourceExhaustedException.class);
+        assertExceptionMapping(sts, StatusCode.Code.FAILED_PRECONDITION, FailedPreconditionException.class);
+        assertExceptionMapping(sts, StatusCode.Code.ABORTED, DeadlineExceededException.class);
+        assertExceptionMapping(sts, StatusCode.Code.OUT_OF_RANGE, InvalidArgumentException.class);
+        assertExceptionMapping(sts, StatusCode.Code.UNIMPLEMENTED, UnSupportedOperationException.class);
+        assertExceptionMapping(sts, StatusCode.Code.INTERNAL, UnknownException.class);
+        assertExceptionMapping(sts, StatusCode.Code.UNAVAILABLE, UnknownException.class);
+        assertExceptionMapping(sts, StatusCode.Code.DATA_LOSS, UnknownException.class);
+        assertExceptionMapping(sts, StatusCode.Code.UNAUTHENTICATED, UnAuthorizedException.class);
+    }
+
+    @Test
+    public void TestGetExceptionWithNonApiException() {
+        GcpSts sts = new GcpSts().builder().build(mockStsClient);
+        Class<? extends SubstrateSdkException> exceptionClass = sts.getException(new RuntimeException("Test error"));
+        Assertions.assertEquals(UnknownException.class, exceptionClass);
+    }
+
+    private void assertExceptionMapping(GcpSts sts, StatusCode.Code statusCode, Class<? extends SubstrateSdkException> expectedExceptionClass) {
+        ApiException apiException = Mockito.mock(ApiException.class);
+        StatusCode mockStatusCode = Mockito.mock(StatusCode.class);
+        Mockito.when(apiException.getStatusCode()).thenReturn(mockStatusCode);
+        Mockito.when(mockStatusCode.getCode()).thenReturn(statusCode);
+        
+        Class<? extends SubstrateSdkException> actualExceptionClass = sts.getException(apiException);
+        Assertions.assertEquals(expectedExceptionClass, actualExceptionClass, 
+            "Expected " + expectedExceptionClass.getSimpleName() + " for status code " + statusCode);
     }
 }
