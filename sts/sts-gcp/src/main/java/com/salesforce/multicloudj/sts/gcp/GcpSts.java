452fc53120cc0353123dc1cb4725f0aebc360dc7
Yamini Kashyap <yaminik1996@gmail.com>
2025-05-30 11:51:11 -0700

    Add conformance testing to GCP STS

--- sts/sts-gcp/src/main/java/com/salesforce/multicloudj/sts/gcp/GcpSts.java
+++ sts/sts-gcp/src/main/java/com/salesforce/multicloudj/sts/gcp/GcpSts.java
@@ -25,6 +25,7 @@ import com.salesforce.multicloudj.sts.model.AssumedRoleRequest;
 import com.salesforce.multicloudj.sts.model.CallerIdentity;
 import com.salesforce.multicloudj.sts.model.GetAccessTokenRequest;
 import com.salesforce.multicloudj.sts.model.StsCredentials;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -57,13 +58,14 @@ public class GcpSts extends AbstractSts<GcpSts> {
 
     @Override
     protected StsCredentials getSTSCredentialsWithAssumeRole(AssumedRoleRequest request){
-        GenerateAccessTokenRequest accessTokenRequest = GenerateAccessTokenRequest.newBuilder()
+        GenerateAccessTokenRequest.Builder accessTokenRequestBuilder = GenerateAccessTokenRequest.newBuilder()
                 .setName("projects/-/serviceAccounts/" + request.getRole())
-                .addAllScope(List.of(scope))
-                .setLifetime(Duration.newBuilder().setSeconds(request.getExpiration()).build())
-                .build();
-        GenerateAccessTokenResponse response = this.stsClient.generateAccessToken(accessTokenRequest);
-        return new StsCredentials(null, null, response.getAccessToken());
+                .addAllScope(List.of(scope));
+        if (request.getExpiration() > 0) {
+            accessTokenRequestBuilder.setLifetime(Duration.newBuilder().setSeconds(request.getExpiration()));
+        }
+        GenerateAccessTokenResponse response = this.stsClient.generateAccessToken(accessTokenRequestBuilder.build());
+        return new StsCredentials(StringUtils.EMPTY, StringUtils.EMPTY, response.getAccessToken());
     }
 
     @Override
@@ -71,7 +73,7 @@ public class GcpSts extends AbstractSts<GcpSts> {
         try {
             GoogleCredentials credentials = GoogleCredentials.getApplicationDefault().createScoped(List.of(scope));
             credentials.refreshIfExpired();
-            return new CallerIdentity(null, credentials.getAccessToken().getTokenValue(), null);
+            return new CallerIdentity(StringUtils.EMPTY, credentials.getAccessToken().getTokenValue(), StringUtils.EMPTY);
         } catch (IOException e) {
             throw new SubstrateSdkException("Could not create credentials in given environment", e);
         }
@@ -82,7 +84,7 @@ public class GcpSts extends AbstractSts<GcpSts> {
         try {
             GoogleCredentials credentials = GoogleCredentials.getApplicationDefault().createScoped(List.of(scope));
             credentials.refreshIfExpired();
-            return new StsCredentials(null, null, credentials.getAccessToken().getTokenValue());
+            return new StsCredentials(StringUtils.EMPTY, StringUtils.EMPTY, credentials.getAccessToken().getTokenValue());
         } catch (IOException e) {
             throw new SubstrateSdkException("Could not create credentials in given environment", e);
         }
@@ -98,29 +100,29 @@ public class GcpSts extends AbstractSts<GcpSts> {
         if (t instanceof ApiException) {
             ApiException exception = (ApiException) t;
             StatusCode statusCode = exception.getStatusCode();
-
-            Map<StatusCode.Code, Class<? extends SubstrateSdkException>> ERROR_MAP = new HashMap<>() {{
-                put(StatusCode.Code.CANCELLED, UnknownException.class);
-                put(StatusCode.Code.UNKNOWN, UnknownException.class);
-                put(StatusCode.Code.INVALID_ARGUMENT, InvalidArgumentException.class);
-                put(StatusCode.Code.DEADLINE_EXCEEDED, DeadlineExceededException.class);
-                put(StatusCode.Code.NOT_FOUND, ResourceNotFoundException.class);
-                put(StatusCode.Code.ALREADY_EXISTS, ResourceAlreadyExistsException.class);
-                put(StatusCode.Code.PERMISSION_DENIED, UnAuthorizedException.class);
-                put(StatusCode.Code.RESOURCE_EXHAUSTED, ResourceExhaustedException.class);
-                put(StatusCode.Code.FAILED_PRECONDITION, FailedPreconditionException.class);
-                put(StatusCode.Code.ABORTED, DeadlineExceededException.class);
-                put(StatusCode.Code.OUT_OF_RANGE, InvalidArgumentException.class);
-                put(StatusCode.Code.UNIMPLEMENTED, UnSupportedOperationException.class);
-                put(StatusCode.Code.INTERNAL, UnknownException.class);
-                put(StatusCode.Code.UNAVAILABLE, UnknownException.class);
-                put(StatusCode.Code.DATA_LOSS, UnknownException.class);
-                put(StatusCode.Code.UNAUTHENTICATED, UnAuthorizedException.class);
-            }};
-
-            return ERROR_MAP.getOrDefault(statusCode.getCode(), UnknownException.class);
+            return ERROR_MAPPING.getOrDefault(statusCode.getCode(), UnknownException.class);
         }
         return UnknownException.class;
+    }
+
+    private static final Map<StatusCode.Code, Class<? extends SubstrateSdkException>> ERROR_MAPPING = new HashMap<>();
+    static {
+        ERROR_MAPPING.put(StatusCode.Code.CANCELLED, UnknownException.class);
+        ERROR_MAPPING.put(StatusCode.Code.UNKNOWN, UnknownException.class);
+        ERROR_MAPPING.put(StatusCode.Code.INVALID_ARGUMENT, InvalidArgumentException.class);
+        ERROR_MAPPING.put(StatusCode.Code.DEADLINE_EXCEEDED, DeadlineExceededException.class);
+        ERROR_MAPPING.put(StatusCode.Code.NOT_FOUND, ResourceNotFoundException.class);
+        ERROR_MAPPING.put(StatusCode.Code.ALREADY_EXISTS, ResourceAlreadyExistsException.class);
+        ERROR_MAPPING.put(StatusCode.Code.PERMISSION_DENIED, UnAuthorizedException.class);
+        ERROR_MAPPING.put(StatusCode.Code.RESOURCE_EXHAUSTED, ResourceExhaustedException.class);
+        ERROR_MAPPING.put(StatusCode.Code.FAILED_PRECONDITION, FailedPreconditionException.class);
+        ERROR_MAPPING.put(StatusCode.Code.ABORTED, DeadlineExceededException.class);
+        ERROR_MAPPING.put(StatusCode.Code.OUT_OF_RANGE, InvalidArgumentException.class);
+        ERROR_MAPPING.put(StatusCode.Code.UNIMPLEMENTED, UnSupportedOperationException.class);
+        ERROR_MAPPING.put(StatusCode.Code.INTERNAL, UnknownException.class);
+        ERROR_MAPPING.put(StatusCode.Code.UNAVAILABLE, UnknownException.class);
+        ERROR_MAPPING.put(StatusCode.Code.DATA_LOSS, UnknownException.class);
+        ERROR_MAPPING.put(StatusCode.Code.UNAUTHENTICATED, UnAuthorizedException.class);
     }
 
     public static class Builder extends AbstractSts.Builder<GcpSts> {
