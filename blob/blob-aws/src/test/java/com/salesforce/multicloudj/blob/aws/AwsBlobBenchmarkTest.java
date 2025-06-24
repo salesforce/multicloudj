715642f9d97dc953fbbefde881144180b75957f3
Harshdeep Kaur <harshdeepk9@gmail.com>
2025-06-24 14:10:08 -0400

    AWS blob benchmark PR

--- blob/blob-aws/src/test/java/com/salesforce/multicloudj/blob/aws/AwsBlobBenchmarkTest.java
+++ blob/blob-aws/src/test/java/com/salesforce/multicloudj/blob/aws/AwsBlobBenchmarkTest.java
@@ -3,11 +3,7 @@ import com.salesforce.multicloudj.blob.client.AbstractBlobBenchmarkTest;
 import com.salesforce.multicloudj.blob.driver.AbstractBlobStore;
 import com.salesforce.multicloudj.common.aws.util.TestsUtilAws;
 import com.salesforce.multicloudj.common.util.common.TestsUtil;
-import com.salesforce.multicloudj.sts.model.CredentialsOverrider;
-import com.salesforce.multicloudj.sts.model.CredentialsType;
-import com.salesforce.multicloudj.sts.model.StsCredentials;
-import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+
 import software.amazon.awssdk.http.SdkHttpClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -26,36 +22,18 @@ import org.openjdk.jmh.runner.RunnerException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Disabled
+//@Disabled
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class AwsBlobBenchmarkTest extends AbstractBlobBenchmarkTest {
 
     private static final Logger logger = LoggerFactory.getLogger(AwsBlobBenchmarkTest.class);
     private static final String endpoint = "https://s3.us-east-2.amazonaws.com";
     private static final String bucketName = "chameleon-jcloud-benchmarks";
-    private static final String versionedBucketName = "chameleon-jcloud-versioned";
-    private static final String nonExistentBucketName = "java-bucket-does-not-exist";
     private static final String region = "us-east-2";
 
     @Override
     protected Harness createHarness() {
         return new HarnessImpl();
-    }
-
-    @Override
-    @Test
-    public void runBenchmarks() throws RunnerException {
-        Options opt = new OptionsBuilder()
-                .include(".*benchmarkSingleActionPut.*")
-                //.include(".*" + this.getClass().getName() + ".*") 
-                .forks(1)
-                .warmupIterations(3)
-                .measurementIterations(5)
-                .resultFormat(org.openjdk.jmh.results.format.ResultFormatType.JSON)
-                .result("target/jmh-results.json")
-                .build();
-
-        new Runner(opt).run();
     }
 
     public static class HarnessImpl implements Harness {
@@ -64,31 +42,9 @@ public class AwsBlobBenchmarkTest extends AbstractBlobBenchmarkTest {
 
         @Override
         public AbstractBlobStore<?> createBlobStore(boolean useValidBucket, boolean useValidCredentials, boolean useVersionedBucket) {
-            String accessKeyId = System.getenv().getOrDefault("AWS_ACCESS_KEY_ID", "FAKE_ACCESS_KEY");
-            String secretAccessKey = System.getenv().getOrDefault("AWS_SECRET_ACCESS_KEY", "FAKE_SECRET_ACCESS_KEY");
-            String sessionToken = System.getenv().getOrDefault("AWS_SESSION_TOKEN", "FAKE_SESSION_TOKEN");
-
-            StsCredentials sessionCreds = new StsCredentials(accessKeyId, secretAccessKey, sessionToken);
-            CredentialsOverrider credentialsOverrider = new CredentialsOverrider.Builder(CredentialsType.SESSION)
-                    .withSessionCredentials(sessionCreds).build();
-            
-            String bucketNameToUse = useValidBucket ? 
-                    (useVersionedBucket ? versionedBucketName : bucketName) : 
-                    nonExistentBucketName;
-
-            return createBlobStore(bucketNameToUse, credentialsOverrider);
-        }
-
-        private AbstractBlobStore<?> createBlobStore(final String bucketName, final CredentialsOverrider credentialsOverrider) {
-            AwsSessionCredentials awsCredentials = AwsSessionCredentials.create(
-                    credentialsOverrider.getSessionCredentials().getAccessKeyId(),
-                    credentialsOverrider.getSessionCredentials().getAccessKeySecret(),
-                    credentialsOverrider.getSessionCredentials().getSecurityToken());
-
-
+            // Use AWS default credential chain (environment variables, IAM roles, etc.)
             client = S3Client.builder()
                     .region(Region.US_EAST_2)
-                    .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
                     .endpointOverride(URI.create(endpoint))
                     .serviceConfiguration(S3Configuration.builder()
                             .pathStyleAccessEnabled(true)
@@ -99,20 +55,9 @@ public class AwsBlobBenchmarkTest extends AbstractBlobBenchmarkTest {
             builder.withS3Client(client)
                     .withEndpoint(URI.create(endpoint))
                     .withBucket(bucketName)
-                    .withRegion(region)
-                    .withCredentialsOverrider(credentialsOverrider);
+                    .withRegion(region);
 
             return builder.build();
-        }
-
-        @Override
-        public String getEndpoint() {
-            return endpoint;
-        }
-
-        @Override
-        public String getProviderId() {
-            return "aws";
         }
 
         @Override
--- blob/blob-client/src/test/java/com/salesforce/multicloudj/blob/client/AbstractBlobBenchmarkTest.java
+++ blob/blob-client/src/test/java/com/salesforce/multicloudj/blob/client/AbstractBlobBenchmarkTest.java
@@ -12,7 +12,19 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.annotations.Threads;
+import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.infra.Blackhole;
 import org.openjdk.jmh.results.format.ResultFormatType;
 import org.openjdk.jmh.runner.Runner;
@@ -23,7 +35,11 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
@@ -65,8 +81,6 @@ public abstract class AbstractBlobBenchmarkTest {
     // Harness interface
     public interface Harness extends AutoCloseable {
         AbstractBlobStore<?> createBlobStore(boolean useValidBucket, boolean useValidCredentials, boolean useVersionedBucket);
-        String getEndpoint();
-        String getProviderId();
         String getBucketName();
     }
 
