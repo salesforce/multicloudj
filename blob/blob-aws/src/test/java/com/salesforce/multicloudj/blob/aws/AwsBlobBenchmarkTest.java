03e9710b1f699053a9a2d9bcf2b7e14a8e460136
Harshdeep Kaur <harshdeep.kaur@salesforce.com>
2025-07-11 14:09:34 -0400

    ammend

--- blob/blob-aws/src/test/java/com/salesforce/multicloudj/blob/aws/AwsBlobBenchmarkTest.java
+++ blob/blob-aws/src/test/java/com/salesforce/multicloudj/blob/aws/AwsBlobBenchmarkTest.java
@@ -20,7 +20,7 @@ import org.openjdk.jmh.runner.RunnerException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-//@Disabled
+@Disabled
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class AwsBlobBenchmarkTest extends AbstractBlobBenchmarkTest {
 
