04592b21dc79bab95a1128e5cd8935fe0dc446aa
Sriram Guduri <shreeram.guduri@gmail.com>
2025-12-01 23:31:13 +0530

    Revert "test: Enable the testTagging (#173)"
    This reverts commit 1e39a62bae4d74cd49547c365ebd277884e91d4d.

--- blob/blob-client/src/test/java/com/salesforce/multicloudj/blob/client/AbstractBlobStoreIT.java
+++ blob/blob-client/src/test/java/com/salesforce/multicloudj/blob/client/AbstractBlobStoreIT.java
@@ -2004,6 +2004,7 @@ public abstract class AbstractBlobStoreIT {
     }
 
     @Test
+    @Disabled
     public void testTagging() throws IOException {
 
         AbstractBlobStore blobStore = harness.createBlobStore(true, true, false);
