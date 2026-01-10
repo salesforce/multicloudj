20bfc4152cc8f3b3a84b8073b275897b40ee468a
Barry Liu <liulihao731@gmail.com>
2026-01-09 23:01:17 -0500

    Fix import2

--- multicloudj-common/src/test/java/com/salesforce/multicloudj/common/exceptions/ExceptionHandlerTest.java
+++ multicloudj-common/src/test/java/com/salesforce/multicloudj/common/exceptions/ExceptionHandlerTest.java
@@ -1,5 +1,7 @@
 package com.salesforce.multicloudj.common.exceptions;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ExceptionHandlerTest {
