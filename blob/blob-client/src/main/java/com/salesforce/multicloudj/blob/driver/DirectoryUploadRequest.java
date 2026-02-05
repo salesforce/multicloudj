f3311255033c454f2b1f179c7059d0f4b52db903
Barry Liu <47010345+LihaoLiuXs@users.noreply.github.com>
2026-02-05 01:05:25 -0500

    blobStore: Directory tagging (#217)

--- blob/blob-client/src/main/java/com/salesforce/multicloudj/blob/driver/DirectoryUploadRequest.java
+++ blob/blob-client/src/main/java/com/salesforce/multicloudj/blob/driver/DirectoryUploadRequest.java
@@ -3,6 +3,8 @@ package com.salesforce.multicloudj.blob.driver;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.Map;
+
 /**
  * Wrapper object for directory upload data
  */
@@ -12,4 +14,8 @@ public class DirectoryUploadRequest {
     private final String localSourceDirectory;
     private final String prefix;
     private final boolean includeSubFolders;
+    /**
+     * (Optional parameter) The map of tagName to tagValue to be associated with all blobs in the directory
+     */
+    private final Map<String, String> tags;
 }
