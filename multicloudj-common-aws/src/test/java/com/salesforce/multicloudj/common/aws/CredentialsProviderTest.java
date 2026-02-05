98cbc7b5849a32be94c9428e4eca49d1329fcb13
sandeepvinayak <sandeep.pal@outlook.com>
2026-02-04 19:25:35 -0800

    sts: fix the aws web identity token provider overrider

--- multicloudj-common-aws/src/test/java/com/salesforce/multicloudj/common/aws/CredentialsProviderTest.java
+++ multicloudj-common-aws/src/test/java/com/salesforce/multicloudj/common/aws/CredentialsProviderTest.java
@@ -9,7 +9,6 @@ import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sts.auth.StsAssumeRoleCredentialsProvider;
-import software.amazon.awssdk.services.sts.auth.StsAssumeRoleWithWebIdentityCredentialsProvider;
 
 public class CredentialsProviderTest {
 
@@ -51,28 +50,42 @@ public class CredentialsProviderTest {
     }
 
     @Test
-    public void testAssumeRoleWebIdentityCredentialsProviderWithSessionName() {
-        CredentialsOverrider credentialsOverrider =
-                new CredentialsOverrider.Builder(CredentialsType.ASSUME_ROLE_WEB_IDENTITY)
-                        .withRole("testRole")
-                        .withWebIdentityTokenSupplier(() -> "mockWebIdentityToken")
-                        .withSessionName("customWebSession")
-                        .build();
-        AwsCredentialsProvider awsCredsProvider =
-                CredentialsProvider.getCredentialsProvider(credentialsOverrider, Region.AF_SOUTH_1);
-        Assertions.assertNotNull(awsCredsProvider);
-        Assertions.assertInstanceOf(RefreshingWebIdentityProvider.class, awsCredsProvider);
-    }
+    public void testAssumeRoleWebIdentityFactoryLambdaInvocation() {
+        // Create a spy to verify the token supplier is called by the lambda
+        final boolean[] tokenSupplierCalled = {false};
 
-    @Test
-    public void testAssumeRoleWebIdentityCredentialsProviderWithoutTokenSupplierThrowsException() {
         CredentialsOverrider credentialsOverrider =
                 new CredentialsOverrider.Builder(CredentialsType.ASSUME_ROLE_WEB_IDENTITY)
-                        .withRole("testRole")
+                        .withRole("arn:aws:iam::123456789012:role/TestRole")
+                        .withWebIdentityTokenSupplier(() -> {
+                            tokenSupplierCalled[0] = true;
+                            return "mockToken123";
+                        })
+                        .withSessionName("testSession")
+                        .withDurationSeconds(3600)
                         .build();
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            CredentialsProvider.getCredentialsProvider(credentialsOverrider, Region.AF_SOUTH_1);
-        });
+
+        AwsCredentialsProvider provider =
+                CredentialsProvider.getCredentialsProvider(credentialsOverrider, Region.US_EAST_1);
+
+        Assertions.assertNotNull(provider);
+        Assertions.assertInstanceOf(RefreshingWebIdentityProvider.class, provider);
+
+        // The token supplier should NOT have been called yet (lazy initialization)
+        Assertions.assertFalse(tokenSupplierCalled[0],
+                "Token supplier should not be called until credentials are resolved");
+
+        // Now invoke the lambda by attempting to resolve credentials
+        // This will fail because we don't have real AWS credentials, but it proves the lambda is invoked
+        try {
+            provider.resolveCredentials();
+        } catch (Exception e) {
+            // Expected to fail, but the token supplier should have been called by the lambda
+        }
+
+        // Verify the lambda was invoked and called the token supplier
+        Assertions.assertTrue(tokenSupplierCalled[0],
+                "Token supplier should have been called when lambda was invoked");
     }
 
     @Test
