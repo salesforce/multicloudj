1f4d2970a0540704d9aed3c89684e82c81d24b87
Rose Yang <122412438+roseyang62@users.noreply.github.com>
2026-03-29 21:48:28 -0700

    test: onboard registry GCP and AWS conformance test (#335)

--- multicloudj-common-aws/src/test/java/com/salesforce/multicloudj/common/aws/util/TestsUtilAws.java
+++ multicloudj-common-aws/src/test/java/com/salesforce/multicloudj/common/aws/util/TestsUtilAws.java
@@ -2,39 +2,18 @@ package com.salesforce.multicloudj.common.aws.util;
 
 import static com.salesforce.multicloudj.common.util.common.TestsUtil.WIREMOCK_HOST;
 
+import com.salesforce.multicloudj.common.util.common.TestsUtil;
 import java.net.URI;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.security.cert.X509Certificate;
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSession;
 import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 import software.amazon.awssdk.http.SdkHttpClient;
 import software.amazon.awssdk.http.apache.ApacheHttpClient;
 import software.amazon.awssdk.http.apache.ProxyConfiguration;
 import software.amazon.awssdk.http.apache.internal.conn.SdkTlsSocketFactory;
 
 public class TestsUtilAws {
-
-  private static TrustManager[] createTrustAllManager() {
-    return new TrustManager[] {
-      new X509TrustManager() {
-        @Override
-        public void checkClientTrusted(X509Certificate[] certs, String authType) {}
-
-        @Override
-        public void checkServerTrusted(X509Certificate[] certs, String authType) {}
-
-        @Override
-        public X509Certificate[] getAcceptedIssuers() {
-          return new X509Certificate[0];
-        }
-      }
-    };
-  }
 
   public static class TrustAllHostNameVerifier implements HostnameVerifier {
     public boolean verify(String hostname, SSLSession session) {
@@ -54,22 +33,17 @@ public class TestsUtilAws {
         ProxyConfiguration.builder()
             .endpoint(URI.create(scheme + "://" + WIREMOCK_HOST + ":" + port))
             .build();
-    TrustManager[] trustAllCerts = createTrustAllManager();
+    TrustManager[] trustAllCerts = TestsUtil.createTrustAllManager();
 
-    try {
-      SSLContext sslContext = SSLContext.getInstance("TLS");
-      sslContext.init(null, trustAllCerts, new SecureRandom());
+    SSLContext sslContext = TestsUtil.createTrustAllSSLContext();
 
-      SdkTlsSocketFactory sslSocketFactory =
-          new SdkTlsSocketFactory(sslContext, new TrustAllHostNameVerifier());
+    SdkTlsSocketFactory sslSocketFactory =
+        new SdkTlsSocketFactory(sslContext, new TrustAllHostNameVerifier());
 
-      return ApacheHttpClient.builder()
-          .proxyConfiguration(proxyConfig)
-          .tlsTrustManagersProvider(() -> trustAllCerts)
-          .socketFactory(sslSocketFactory)
-          .build();
-    } catch (NoSuchAlgorithmException | KeyManagementException e) {
-      throw new RuntimeException(e);
-    }
+    return ApacheHttpClient.builder()
+        .proxyConfiguration(proxyConfig)
+        .tlsTrustManagersProvider(() -> trustAllCerts)
+        .socketFactory(sslSocketFactory)
+        .build();
   }
 }
