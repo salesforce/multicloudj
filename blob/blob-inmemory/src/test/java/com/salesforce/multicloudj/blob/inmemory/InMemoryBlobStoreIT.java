f2738a2249c5eb7bbcebe720a3dfc1fd260b597e
Barry Liu <47010345+LihaoLiuXs@users.noreply.github.com>
2026-04-14 20:19:07 -0400

    blobstore: support isObjectLockSupported in in memory blobstore (#380)

--- blob/blob-inmemory/src/test/java/com/salesforce/multicloudj/blob/inmemory/InMemoryBlobStoreIT.java
+++ blob/blob-inmemory/src/test/java/com/salesforce/multicloudj/blob/inmemory/InMemoryBlobStoreIT.java
@@ -83,7 +83,7 @@ public class InMemoryBlobStoreIT extends AbstractBlobStoreIT {
 
     @Override
     public boolean isObjectLockSupported() {
-      return false;
+      return true;
     }
 
     @Override
