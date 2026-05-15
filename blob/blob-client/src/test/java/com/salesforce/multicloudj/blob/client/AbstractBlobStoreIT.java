56cc6fe3e0b3468178a23a8d0d8a18c697af5a42
Kim Choy <kchoy@salesforce.com>
2026-05-15 10:59:24 -0700

    blobstore: disable testDownload_checkArchived conformance tests for Ali

--- blob/blob-client/src/test/java/com/salesforce/multicloudj/blob/client/AbstractBlobStoreIT.java
+++ blob/blob-client/src/test/java/com/salesforce/multicloudj/blob/client/AbstractBlobStoreIT.java
@@ -4786,6 +4786,7 @@ public abstract class AbstractBlobStoreIT {
 
   @Test
   public void testDownload_checkArchived() throws IOException {
+    Assumptions.assumeFalse(ALI_PROVIDER_ID.equals(harness.getProviderId()));
     AbstractBlobStore blobStore = harness.createBlobStore(true, true, true);
     BucketClient bucketClient = new BucketClient(blobStore);
     String key = "conformance-tests/check-archived/archived-blob";
@@ -4830,6 +4831,7 @@ public abstract class AbstractBlobStoreIT {
 
   @Test
   public void testDownload_checkArchived_neverExisted() throws IOException {
+    Assumptions.assumeFalse(ALI_PROVIDER_ID.equals(harness.getProviderId()));
     AbstractBlobStore blobStore = harness.createBlobStore(true, true, true);
     BucketClient bucketClient = new BucketClient(blobStore);
 
